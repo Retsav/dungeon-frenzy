d60fcea530ebe87aee4afc8a5351da267b242563
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-10-28 18:28:40 +0200

    Added Unit transform to mouse position

--- Assets/Scripts/MouseWorld.cs
+++ Assets/Scripts/MouseWorld.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 
 public class MouseWorld : MonoBehaviour
 {
-    private void Update()
+    private static MouseWorld instance;
+    
+    [Header("Settings")]
+    [SerializeField] private LayerMask mousePlaneLayerMask;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    
+    public static Vector3 GetPosition()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray);
+        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.mousePlaneLayerMask);
+        return raycastHit.point;
     }
 }
