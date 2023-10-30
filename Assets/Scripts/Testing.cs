1fa7f4a5d56da4a6cb2cb42a15ff5b38ac9a4855
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-10-30 18:37:56 +0100

    Edited testing script

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -5,14 +5,5 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-    private GridSystem gridSystem;
-    private void Start()
-    {
-        gridSystem = new GridSystem(10, 10, 2f);
-    }
 
-    private void Update()
-    {
-        Debug.Log(gridSystem.GetGridPosition(MouseWorld.GetPosition()));
-    }
 }
