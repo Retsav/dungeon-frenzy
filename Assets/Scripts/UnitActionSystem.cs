65cfbba3097cd1377cdda8871044313708053a8f
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-11-01 16:39:03 +0100

    Added onactionchanged event to UnitActionSystem

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -8,6 +8,7 @@ public class UnitActionSystem : MonoBehaviour
 {
     public static UnitActionSystem Instance { get; private set; }
     public event EventHandler OnSelectedUnitChanged;
+    public event EventHandler OnSelectedActionChanged;
     [SerializeField] private Unit selectedUnit;
     [Header("Settings")]
     [SerializeField] private LayerMask unitLayer;
@@ -87,6 +88,7 @@ public class UnitActionSystem : MonoBehaviour
     public void SetSelectedAction(BaseAction baseAction)
     {
         selectedAction = baseAction;
+        OnSelectedActionChanged?.Invoke(this, EventArgs.Empty);
     }
 
     private void HandleSelectedAction()
