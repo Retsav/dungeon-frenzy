9ecff4f9adc40971d8232ad3be95884a4a763560
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-11-03 13:09:30 +0100

    Added basic enemy turn logic

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -40,7 +40,10 @@ public class UnitActionSystem : MonoBehaviour
         {
             return;
         }
-
+        if (TurnSystem.Instance.IsPlayerTurn())
+        {
+            return;
+        }
         if (EventSystem.current.IsPointerOverGameObject())
         {
             return;
