71cacb97cfe8efe5e162d162e44de7c4e465c0dd
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-11-12 14:05:54 +0100

    Previous actions code refactoring

--- Assets/Scripts/Actions/MoveAction.cs
+++ Assets/Scripts/Actions/MoveAction.cs
@@ -36,8 +36,7 @@ public class MoveAction : BaseAction
         else
         {
             unitAnimator.SetBool("IsWalking", false);
-            isActive = false;
-            onActionComplete();
+            ActionComplete();
         }
         float rotateSpeed = 10f;
         transform.forward = Vector3.Lerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
@@ -45,9 +44,8 @@ public class MoveAction : BaseAction
     
     public override void TakeAction(GridPosition gridPosition, Action onActionComplete)
     {
-        this.onActionComplete = onActionComplete;
+        ActionStart(onActionComplete);
         this.targetPosition = LevelGrid.Instance.GetWorldPosition(gridPosition);
-        isActive = true;
     }
     
     public override List<GridPosition> GetValidActionGridPositionList()
